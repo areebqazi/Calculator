d9850f0600e3007cc6fba3af00b95bff36090eaf
areeb <areebqzi@gmail.com>
2023-03-03 22:39:29 +0530

    simple calculator

---
+++ index.js
@@ -0,0 +1,20 @@
+let a = 8
+let b = 5
+document.getElementById("num1-el").textContent=a
+document.getElementById("num2-el").textContent=b
+
+let temp=document.getElementById("sum-el")
+
+function add(){
+     temp.textContent="Result : "+a+b
+}
+function subtract(){
+    temp.textContent="Result : "+(a-b)
+}
+function divide(){
+    temp.textContent="Result : "+a/b
+}
+function multiply(){
+    temp.textContent="Result : "+a*b
+}
+
